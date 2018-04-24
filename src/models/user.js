c481ded186c3700c8e1ceb0246f9d7644f43f7eb
Lukas Lindqvist <mail@lukaslindqvist.se>
2018-04-24 16:19:25 +0200

    Fixed missing commas.

--- src/models/user.js
+++ src/models/user.js
@@ -3,7 +3,7 @@ export default (sequelize, DataTypes) => {
     id: {
       type: DataTypes.UUID,
       primaryKey: true,
-      defaultValue: DataTypes.UUIDV4
+      defaultValue: DataTypes.UUIDV4,
       allowNull: false,
       autoIncrement: false,
     },
